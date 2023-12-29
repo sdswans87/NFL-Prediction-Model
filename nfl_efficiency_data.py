0a4f07eb2f16b3ce9afe8d49dbc26f57c6c547e9
Scott <swan0332@gmail.com>
2023-12-29 07:11:18 -0600

    sdswans - epa/efficiency development 12/29

--- nfl_efficiency_data.py
+++ nfl_efficiency_data.py
@@ -4,6 +4,7 @@ Created on Wed Dec 27 06:24:49 2023
 
 @author: swan0
 """
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import time
@@ -36,6 +37,56 @@ class efficiency_data_class():
         # join pass/run effciency
         self.off_epa_efficiency = self.pos_epa_efficiency_func(self.pass_efficiency, self.run_efficiency, 'posteam')
         self.def_epa_efficiency = self.pos_epa_efficiency_func(self.pass_efficiency_allowed, self.run_efficiency_allowed,'defteam')
+
+        # points per game
+        self.points_per_gm = self.points_per_game_func(epa_obj.close_game_data, 'posteam')
+        self.points_per_gm_allowed = self.points_per_game_func(epa_obj.close_game_data, 'defteam')        
+
+        # join offensive data with points data.
+        self.off_epa_efficiency = self.join_eff_points_func(self.off_epa_efficiency, self.points_per_gm, 'posteam')
+        self.def_epa_efficiency = self.join_eff_points_func(self.def_epa_efficiency, self.points_per_gm_allowed, 'defteam')
+
+        # add to epa stats
+        self.off_efficiency_adj = self.cpoe_func(epa_obj.close_game_data, self.off_epa_efficiency, 'posteam')
+        self.def_efficiency_adj = self.cpoe_func(epa_obj.close_game_data, self.def_epa_efficiency, 'defteam')
+
+        # graph pass/run epa
+        self.pass_epa_graph(self.off_efficiency_adj)
+        self.run_epa_graph(self.off_efficiency_adj)
+
+        # first down offensive efficiency 
+        self.first_down_off_pass = self.down_efficiency_func(epa_obj.close_game_data, 1, 'pass', 'posteam')
+        self.first_down_off_rush = self.down_efficiency_func(epa_obj.close_game_data, 1,'run', 'posteam')
+
+        # first down defensive efficiency
+        self.first_down_def_pass = self.down_efficiency_func(epa_obj.close_game_data, 1, 'pass', 'defteam')
+        self.first_down_def_rush = self.down_efficiency_func(epa_obj.close_game_data, 1, 'run', 'defteam')
+        
+        # second down offensive efficiency
+        self.second_down_off_pass = self.down_efficiency_func(epa_obj.close_game_data, 2, 'pass', 'posteam')
+        self.second_down_off_rush = self.down_efficiency_func(epa_obj.close_game_data, 2, 'run', 'posteam')
+        
+        # second down defensive efficiency
+        self.second_down_def_pass = self.down_efficiency_func(epa_obj.close_game_data, 2, 'pass', 'defteam')
+        self.second_down_def_rush = self.down_efficiency_func(epa_obj.close_game_data, 2, 'run', 'defteam')
+
+        # third down offensive efficiency
+        self.third_down_off_pass = self.down_efficiency_func(epa_obj.close_game_data, 3, 'pass', 'posteam')
+        self.third_down_off_rush = self.down_efficiency_func(epa_obj.close_game_data, 3, 'run', 'posteam')
+        
+        # third down defensive efficiency
+        self.third_down_def_pass = self.down_efficiency_func(epa_obj.close_game_data, 3, 'pass', 'defteam')
+        self.third_down_def_rush = self.down_efficiency_func(epa_obj.close_game_data, 3, 'run', 'defteam')
+
+        # all down efficiency
+        self.all_down_off_eff = self.all_down_efficiency_func(self.first_down_off_pass, self.first_down_off_rush, 
+                                                              self.second_down_off_pass, self.second_down_off_rush,
+                                                              self.third_down_off_pass, self.third_down_off_rush,
+                                                              "posteam")
+        self.all_down_def_eff = self.all_down_efficiency_func(self.first_down_def_pass, self.first_down_def_rush, 
+                                                              self.second_down_def_pass, self.second_down_def_rush,
+                                                              self.third_down_def_pass, self.third_down_def_rush,
+                                                              "defteam")
 
         # end runtime
         end_run = time.time()
@@ -97,4 +148,100 @@ class efficiency_data_class():
         pos_epa = pos_epa.dropna()
         pos_epa['n_plays'] = pos_epa['n_pass'] + pos_epa['n_rush']
         pos_epa[['year', 'week', 'away_team', 'home_team']] = pos_epa['game_id'].str.split('_', expand=True)
+        temp_dict = {"OAK":"LV", "SD":"LAC"}
+        for key,value in temp_dict.items():
+            pos_epa.loc[pos_epa["home_team"] == key, "home_team"] = value
+            pos_epa.loc[pos_epa["away_team"] == key, "away_team"] = value
         return pos_epa
+    
+
+    def points_per_game_func(self, nfl_data, pos_def):
+        nfl_data['eff_fg_pts'] = nfl_data['fg_prob'] * 3 * nfl_data['field_goal_attempt']
+        pts_per_game = nfl_data.groupby(['game_id', pos_def]).agg(
+                                home_final=('home_score', 'max'),
+                                away_final=('away_score', 'max'),
+                                home_team=('home_team', "max"),
+                                away_team=('away_team', "max"),
+                                total_tds=('touchdown', 'sum'),
+                                total_fgs_att=('field_goal_attempt', 'sum'),
+                                total_pat=('extra_point_attempt', 'sum'),
+                                total_eff_fg_pts=('eff_fg_pts', 'sum')
+                                ).reset_index()
+        pts_per_game["total_effective_pts"] = pts_per_game["total_tds"] * 6 + pts_per_game["total_eff_fg_pts"]
+        pts_per_game['total_score'] = pts_per_game['home_final'] + pts_per_game['away_final']
+        pts_per_game['pt_diff'] = pts_per_game['home_final'] - pts_per_game['away_final']
+        pts_per_game = pts_per_game.loc[pts_per_game[pos_def].notna()].drop_duplicates()
+        return pts_per_game
+    
+
+    def correct_tm_names(self, temp_df):
+        temp_dict = {"OAK":"LV", "SD":"LAC"}
+        for key,value in temp_dict.items():
+            temp_df.loc[temp_df["home_team"] == key, "home_team"] = value
+            temp_df.loc[temp_df["away_team"] == key, "away_team"] = value
+        return temp_df
+    
+
+    def join_eff_points_func(self, eff_data, points_data, pos_def):
+        eff_points = pd.merge(eff_data, points_data, on=['game_id', pos_def, 'home_team', 'away_team'], how='left')
+        if pos_def == 'posteam':
+            eff_points['poss_score'] = pd.NA
+            for row in range(len(eff_points)):
+                if eff_points['posteam'][row] == eff_points['home_team'][row]:
+                    eff_points['poss_score'][row] = eff_points['home_final'][row]
+                else:
+                    eff_points['poss_score'][row] = eff_points['away_final'][row]
+        if pos_def == 'defteam':
+            eff_points['score_allowed'] = pd.NA
+            for row in range(len(eff_points)):
+                if eff_points['defteam'][row] == eff_points['home_team'][row]:
+                    eff_points['score_allowed'][row] = eff_points['home_final'][row]
+                else:
+                    eff_points['score_allowed'][row] = eff_points['away_final'][row]
+        return eff_points
+
+
+    def cpoe_func(self, nfl_data, epa_data, pos_def):
+        epa = nfl_data[nfl_data['cpoe'].notna()]
+        epa = epa.groupby(['game_id', pos_def]).agg(avg_cpoe=('cpoe', 'mean'), total_cpoe=('cpoe', 'sum')).reset_index()
+        out = pd.merge(epa_data, epa, on=['game_id', pos_def], how='left').drop_duplicates()
+        return out
+    
+
+    def pass_epa_graph(self, temp_df):
+        plt.hist(temp_df['pass_epa_game'], bins=range(-40, 41, 1))
+        plt.xlim(-40, 40)
+        plt.title('Pass EPA Per Game')
+        plt.show()
+        
+    def run_epa_graph(self, temp_df):
+        plt.hist(temp_df['rush_epa_game'], bins=range(-25, 26, 1))
+        plt.xlim(-25, 25)
+        plt.title('Run EPA Per Game')
+        plt.show()
+
+
+    def down_efficiency_func(self, nfl_data, dwn, pass_run, pos_def):
+        down = nfl_data[nfl_data['down'] == dwn]
+        down_eff = down[down['play_type'] == pass_run].groupby(['game_id', pos_def]).agg(
+            sum_epa_play_type=('epa', 'sum'),
+            total_success=('success', 'sum'),
+            n_play=('play', 'count')
+            ).reset_index()
+        down_eff["epa_per_play"] = down_eff["sum_epa_play_type"] / down_eff["n_play"]
+        down_eff["succ_rate_play"] = down_eff["total_success"] / down_eff["n_play"]
+        down_eff.rename(columns={'sum_epa_play_type': 'sum_epa_play_type_' + pass_run + "_" + str(dwn),
+                                 'total_success': 'total_success_' + pass_run + "_" + str(dwn),
+                                 'n_play': 'n_play_' + pass_run + "_" + str(dwn),
+                                 'epa_per_play': 'epa_per_play_' + pass_run + "_" + str(dwn),
+                                 'succ_rate_play': 'succ_rate_play_' + pass_run + "_" + str(dwn)}, inplace=True)
+        return down_eff
+    
+
+    def all_down_efficiency_func(self, df1, df2, df3, df4, df5, df6, pos_def):
+        all_down_eff = df1.merge(df2, on = ['game_id',pos_def], how = 'left') 
+        all_down_eff = all_down_eff.merge(df3, on = ['game_id',pos_def], how = 'left') 
+        all_down_eff = all_down_eff.merge(df4, on = ['game_id',pos_def], how = 'left') 
+        all_down_eff = all_down_eff.merge(df5, on = ['game_id',pos_def], how = 'left') 
+        all_down_eff = all_down_eff.merge(df6, on = ['game_id',pos_def], how = 'left') 
+        return all_down_eff
