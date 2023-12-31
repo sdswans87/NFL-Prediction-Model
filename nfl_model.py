5f16f567985ceea70524b80836aa004d814dc4c2
Scott <swan0332@gmail.com>
2023-12-31 06:24:00 -0600

    sdswans - model development 12/31

--- nfl_model.py
+++ nfl_model.py
@@ -24,18 +24,18 @@ class model_data_class():
         start_run = time.time()
 
         # ensemble column lists
-        off_columns = ['game_id','year', 'week', 'away_team', 'home_team','posteam',
-                       'n_pass','n_rush','n_plays', 'home_final','away_final',
-                       'total_tds','total_fgs_att','total_pat','total_effective_pts',
-                       'total_score','pt_diff']
-        def_columns = ['game_id','year', 'week', 'away_team', 'home_team','defteam',
-                       'n_pass','n_rush','n_plays', 'home_final','away_final',
-                       'total_tds','total_fgs_att','total_pat','total_effective_pts',
-                       'total_score','pt_diff']
+        self.off_columns = ['game_id','year', 'week', 'away_team', 'home_team','posteam',
+                            'n_pass','n_rush','n_plays', 'home_final','away_final',
+                            'total_tds','total_fgs_att','total_pat','total_effective_pts',
+                            'total_score','pt_diff']
+        self.def_columns = ['game_id','year', 'week', 'away_team', 'home_team','defteam',
+                            'n_pass','n_rush','n_plays', 'home_final','away_final',
+                            'total_tds','total_fgs_att','total_pat','total_effective_pts',
+                            'total_score','pt_diff']
         
         # prep for model calculation
-        self.ensemble_offense = self.prep_data_func(eff_obj.total_off_efficiency, off_columns)
-        self.ensemble_defense = self.prep_data_func(eff_obj.total_def_efficiency, def_columns)
+        self.ensemble_offense = self.prep_data_func(eff_obj.total_off_efficiency, self.off_columns)
+        self.ensemble_defense = self.prep_data_func(eff_obj.total_def_efficiency, self.def_columns)
 
         # ensemble model offensive efficiency/actual points scored
         h2o.init()
@@ -107,8 +107,8 @@ class model_data_class():
         self.defensive_ensemble_test = self.model_performance_func(self.defensive_ensemble)
         self.offensive_ensemble_test.rmse()
         self.defensive_ensemble_test.rmse()
-        min([self.offensive_gbm_test.rmse(), self.offensive_rf_test.rmse(), self.offensive_lr_test.rmse(), self.offensive_nn_test.rmse()])
-        min([self.defensive_gbm_test.rmse(), self.defensive_rf_test.rmse(), self.defensive_lr_test.rmse(), self.defensive_nn_test.rmse()])
+        print(min([self.offensive_gbm_test.rmse(), self.offensive_rf_test.rmse(), self.offensive_lr_test.rmse(), self.offensive_nn_test.rmse()]))
+        print(min([self.defensive_gbm_test.rmse(), self.defensive_rf_test.rmse(), self.defensive_lr_test.rmse(), self.defensive_nn_test.rmse()]))
 
         # end runtime
         end_run = time.time()
@@ -173,18 +173,14 @@ class model_effective_points():
         start_time = time.time()
 
         # model efficiency columns
-        off_columns = ['game_id','year', 'week', 'away_team', 'home_team','posteam',
-                       'n_pass','n_rush','n_plays', 'home_final','away_final',
-                       'total_tds','total_fgs_att','total_pat','total_score',
-                       'pt_diff']
-        def_columns = ['game_id','year', 'week', 'away_team', 'home_team','defteam',
-                       'n_pass','n_rush','n_plays', 'home_final','away_final',
-                       'total_tds','total_fgs_att','total_pat','total_score',
-                       'pt_diff']
+        columns = ['game_id','year', 'week', 'away_team', 'home_team','posteam',
+                   'n_pass','n_rush','n_plays', 'home_final','away_final',
+                   'total_tds','total_fgs_att','total_pat','total_score',
+                   'pt_diff']
 
         # build models off of effective points
-        self.ensemble_off_eff = mod_obj.prep_data_func(eff_obj.total_off_efficiency, off_columns)
-        self.ensemble_def_eff = mod_obj.prep_data_func(eff_obj.total_def_efficiency, def_columns)
+        self.ensemble_off_eff = mod_obj.prep_data_func(eff_obj.total_off_efficiency, columns)
+        self.ensemble_def_eff = mod_obj.prep_data_func(eff_obj.total_off_efficiency, columns)
         self.ensemble_off_eff_train = mod_obj.ensemble_func(self.ensemble_off_eff, 0)
         self.ensemble_off_eff_test = mod_obj.ensemble_func(self.ensemble_off_eff, 1)
         self.ensemble_def_eff_train = mod_obj.ensemble_func(self.ensemble_def_eff, 0)
@@ -245,9 +241,95 @@ class model_effective_points():
         self.defensive_eff_ensemble_test = mod_obj.model_performance_func(self.defensive_eff_ensemble)
         
         # calculate mins
-        min([self.offensive_eff_gbm_test.rmse(), self.offensive_eff_rf_test.rmse(), self.offensive_eff_lr_test.rmse(), 
-             self.offensive_eff_nn_test.rmse()])
-        min([self.defensive_eff_gbm_test.rmse(), self.defensive_eff_rf_test.rmse(), self.defensive_eff_lr_test.rmse(), 
-             self.defensive_eff_nn_test.rmse()])
+        print(min([self.offensive_eff_gbm_test.rmse(), self.offensive_eff_rf_test.rmse(), self.offensive_eff_lr_test.rmse(), 
+                   self.offensive_eff_nn_test.rmse()]))
+        print(min([self.defensive_eff_gbm_test.rmse(), self.defensive_eff_rf_test.rmse(), self.defensive_eff_lr_test.rmse(), 
+                   self.defensive_eff_nn_test.rmse()]))
         self.offensive_eff_ensemble_test.rmse()
         self.defensive_eff_ensemble_test.rmse()
+
+        # create final offense/defense efficiency 
+        self.final_off = eff_obj.total_offense_eff[["game_id", "posteam", "total_effective_pts"]]
+        self.final_def = eff_obj.total_defense_eff[["game_id", "defteam", "total_effective_pts"]]
+
+        # run off models on newer data
+        ensemble_off = mod_obj.prep_data_func(eff_obj.total_offense_eff, mod_obj.off_columns + ["poss_score"])
+        self.ensemble_off = mod_obj.ensemble_func(ensemble_off, 2)
+
+        # run off models on newer data
+        ensemble_def = mod_obj.prep_data_func(eff_obj.total_defense_eff, mod_obj.def_columns + ["score_allowed"])
+        self.ensemble_def = mod_obj.ensemble_func(ensemble_def, 2)
+
+        # set offensive targets and predictors
+        preds_off = self.offensive_eff_ensemble.predict(self.ensemble_off)
+        self.preds_off = preds_off.as_data_frame()
+        
+        # set defensive targets and predictors
+        preds_def = self.defensive_eff_ensemble.predict(self.ensemble_def)
+        self.preds_def = preds_def.as_data_frame()
+
+        # set final targets and predictors
+        self.final_offense = pd.concat([self.final_off, self.preds_off], axis=1)
+        self.final_defense = pd.concat([self.final_def, self.preds_def], axis=1)
+        self.final_offense.columns.values[3] = 'predicted_points'
+        self.final_defense.columns.values[3] = 'predicted_points_conceded'
+
+        # calculate offensvie adjustment
+        self.rmse_offense = np.sqrt(mean_squared_error(self.final_offense['total_effective_pts'], self.final_offense['predicted_points']))
+        self.rmse_defense = np.sqrt(mean_squared_error(self.final_defense['total_effective_pts'], self.final_defense['predicted_points_conceded']))
+
+        # correct for kicking efficiency
+        self.nfl_fg_stats = self.nfl_fg_efficiency_func(eff_obj.model_data, 'posteam')
+        self.nfl_kickers_2022 = self.nfl_kickers_func(eff_obj.model_data)
+        self.final_off = self.final_offensive_func(self.final_offense, self.nfl_fg_stats)
+
+        # schedule adjustment
+        self.epa_offense = self.epa_offense_func(eff_obj.model_data)
+        self.epa_defense = self.epa_defense_func(eff_obj.model_data)
+
+  
+    def nfl_fg_efficiency_func(self, df, pos_def):
+        df2 = df[df['field_goal_result'].notna()]
+        df2['exp_pts'] = df2['fg_prob'] * 3
+        df2['act_pts'] = np.where(df2['field_goal_result'] == 'made', 3, 0)
+        df2['added_pts'] = df2['act_pts'] - df2['exp_pts']
+        out = df2.groupby(['season', pos_def]).agg(
+            total_added_pts=('added_pts', 'sum'),
+            total_kicks=('field_goal_result', 'count'),
+            kicks_per_game=('field_goal_result', lambda x: len(x) / 17),
+            avg_kick_exp_pts=('exp_pts', 'mean'),
+            avg_pts_per_kick=('act_pts', 'mean'),
+            add_pts_per_kick=('added_pts', 'mean'),
+            add_pts_per_game=('added_pts', lambda x: sum(x) / 17)
+        ).reset_index()
+        return out
+
+
+    def nfl_kickers_func(self, temp_df):
+        nfl_kickers_2022 = temp_df[temp_df['field_goal_attempt'] == 1][['posteam', 'kicker_player_name', 'kicker_player_id']]
+        nfl_kickers_2022 = nfl_kickers_2022.dropna(subset=['kicker_player_id']).drop_duplicates()
+        return nfl_kickers_2022
+    
+    
+    def final_offensive_func(self, df1, df2):
+        df1['kick_pts_add'] = df1['posteam'].map(df2.set_index('posteam')['add_pts_per_game'])
+        df1['adj_pts'] = df1['total_effective_pts'] + df1['kick_pts_add']
+        return df1
+
+
+    def epa_offense_func(self, temp_df):
+        epa_off = temp_df[(~temp_df['epa'].isna()) & (~temp_df['posteam'].isna()) & (temp_df['play_type'].isin(['pass', 'run']))]
+        epa_off = epa_off.groupby(['game_id', 'week', 'posteam']).agg(off_epa=('epa', 'mean'), off_plays=('play_type', 'count')).reset_index()
+        epa_off['off_epa'] = epa_off.groupby('posteam')['off_epa'].transform(lambda x: x.rolling(window=14, min_periods=1).mean().shift())
+        epa_off['off_plays'] = epa_off.groupby('posteam')['off_plays'].transform(lambda x: x.rolling(window=14, min_periods=1).mean().shift())
+        epa_off = epa_off.sort_values('week').reset_index(drop=True)
+        return epa_off
+    
+ 
+    def epa_defense_func(self, temp_df):
+        epa_def = temp_df[(~temp_df['epa'].isna()) & (~temp_df['defteam'].isna()) & (temp_df['play_type'].isin(['pass', 'run']))]
+        epa_def = epa_def.groupby(['game_id', 'week', 'defteam']).agg(def_epa=('epa', 'mean'), def_plays=('play_type', 'count')).reset_index()
+        epa_def['def_epa'] = epa_def.groupby('defteam')['def_epa'].transform(lambda x: x.rolling(window=14, min_periods=1).mean().shift())
+        epa_def['def_plays'] = epa_def.groupby('defteam')['def_plays'].transform(lambda x: x.rolling(window=14, min_periods=1).mean().shift())
+        epa_def = epa_def.sort_values('week').reset_index(drop=True)
+        return epa_def
